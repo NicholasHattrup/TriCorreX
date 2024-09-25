3914625567f849141945da0c2517d0182fdc64ad
NicholasHattrup <nhattrup@andrew.cmu.edu>
2024-09-25 17:03:53 -0400

    y

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='TriCorreX',               
+    version='0.1',                    
+    description='Fast analysis of 3-body triplet configurations in molecular dynamics systems',   
+    packages=find_packages(),         
+    install_requires=[],              
+)
+
